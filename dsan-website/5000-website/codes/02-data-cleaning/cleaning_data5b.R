d48d0ccb277bda5a3b79581ae235694a149dde17
Kendall Gilbert <kendallgilbert@Kendalls-MacBook-Pro.local>
2023-11-01 15:26:41 -0400

    Major Changes to Naive Bayes and Feature Selection for Record Data

--- dsan-website/5000-website/codes/02-data-cleaning/cleaning_data5b.R
+++
@@ -1,4 +0,0 @@
-#Reading in Data 
-data5b <- read.csv("~/dsan-5000-project-kendallgilbert180/dsan-website/5000-website/data/00-raw-data/5b.csv")
-
-colnames(data5b) #CSV Format is Very Odd with lots of overlapping columns
